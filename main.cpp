057f1071348afee018a022606e2ce5c8a7ec03e8
kaustikos <petrov@poi.dvo.ru>
2015-03-22 01:06:11 +1000

    My first COMMIT!!!! YEAH!
    Richarson extrapolation

--- main.cpp
+++ main.cpp
@@ -28,8 +28,9 @@ struct layer
 
 
 
-vector<double> compute_trapped_modes(double &omeg, vector<double> &c, vector<double> &rho, vector<int> &interface_idcs, vector<double> &meshsizes);
+vector<double> compute_wnumbers(double &omeg, vector<double> &c, vector<double> &rho, vector<unsigned> &interface_idcs, vector<double> &meshsizes,unsigned flOnlyTrapped);
 
+vector<double> compute_wnumbers_extrap(double &omeg, vector<double> &depths,vector<double> &c1s,vector<double> &c2s,vector<double> &rhos,vector<unsigned> &Ns_points, unsigned flOnlyTrapped,unsigned &ordRich);
 
 int main( int argc, char **argv )
 {
@@ -40,22 +41,21 @@ int main( int argc, char **argv )
     double rho_w = 1;
     double rho_b = 2;
     double dz = 1;
-    int nz = 201;
-    int ib = 90;    //at POINT = 89 we have water, at POINT = 90 we have bottom
+    unsigned nz = 501;
+    unsigned ib = 90;    //at POINT = 89 we have water, at POINT = 90 we have bottom
                     //   ii = 89,                  at ii = 90
+
+    cout.precision(15);
 
     double omeg = 2*3.141592653589793*freq;
 
     vector<double> input_c;
     vector<double> input_rho;
-    vector<double> input_mesh;
-	input_mesh.push_back( dz );
-	input_mesh.push_back( dz );
-    vector<int> input_interf_idcs;
-	input_interf_idcs.push_back( ib );
+    vector<double> input_mesh { dz,dz };
+    vector<unsigned> input_interf_idcs { ib };
     vector<double> out_wnum;
 
-    for ( int ii = 0; ii<nz; ii++ ){
+    for ( unsigned ii = 0; ii<nz; ii++ ){
         if (ii<ib){                   //
             input_c.push_back(c_w);
             input_rho.push_back(rho_w);
@@ -70,22 +70,182 @@ int main( int argc, char **argv )
     cout << "freq = " << freq << endl;
     cout << "omega = " << omeg << endl;
 
-    out_wnum = compute_trapped_modes(omeg, input_c, input_rho,input_interf_idcs, input_mesh);
+/*    out_wnum = compute_wnumbers(omeg, input_c, input_rho,input_interf_idcs, input_mesh,1);
 
-	for (unsigned ii=0; ii<out_wnum.size();  ii++)
-		cout << ii << "->" << out_wnum.at(ii) << endl;
+   for (unsigned ii=0; ii<out_wnum.size();  ii++) {
+
+            cout << ii << "->" << out_wnum.at(ii) << endl;
+
+
+    }
+*/
+    cout << "NEW: extrapolation" << endl;
+
+    vector<double> depths {90,500};
+    vector<double> c1s  {1500,2000};
+    vector<double> c2s  {1500,2000};
+    vector<double> rhos  {1,2};
+    vector<unsigned> Ns_points  {90,410};
+    unsigned rord = 4;
+
+    out_wnum = compute_wnumbers_extrap(omeg,depths,c1s,c2s,rhos,Ns_points,1,rord);
+
+    cout << "Extrapolated ev:" << endl;
+    for (unsigned ii=0; ii<out_wnum.size();  ii++) {
+
+            cout << ii << "->" << out_wnum.at(ii) << endl;
+
+
+    }
 
 	return 0;
+
+
 }
 
 
 
 
-vector<double> compute_trapped_modes(           double &omeg, // sound frequency
+vector<double> compute_wnumbers_extrap(       double &omeg, // sound frequency
+											  vector<double> &depths,
+											  vector<double> &c1s,
+											  vector<double> &c2s,
+											  vector<double> &rhos,
+											  vector<unsigned> &Ns_points,
+											  unsigned flOnlyTrapped,
+											  unsigned &ordRich)
+/*  subroutine for computing wavenumbers for a given waveguide structure
+    the computation is performed by the FD method for certain meshsize,
+    Richardson extrapolation is used to improve the
+
+
+Layer structure:
+
+depths_{i-1}----c=c1s_i--
+*
+*
+... <-Ns_points_i       rho = rhos_i
+*
+*
+depths_{i}------c=c2s_i--
+
+Other parameters:
+omeg -- cyclic frequency, omeg = 2*Pi*f;
+flOnlyTrapped -- flag to determine the mode subset: set to 0 to compute all propagating modes, i.e. such that k^2>=0, otherwise only trapped modes are computed
+ordRich -- order of the Richardson extrapolation;
+
+the top of the first layer is z=0
+	*/
+
+
+{
+    vector<double> coeff_extrap;
+    if (ordRich == 1) {
+        coeff_extrap.assign({1});
+    }
+    else if (ordRich == 2){
+        coeff_extrap.assign({-1,2});
+    }
+    else if (ordRich == 3){
+        coeff_extrap.assign({0.5, -4, 4.5});
+    }
+    else if (ordRich == 4){
+        coeff_extrap.assign({-1/double(6),4,-13.5,32/double(3)});
+    }
+    else {
+        ordRich = 3;
+        coeff_extrap.assign({0.5, -4, 4.5});
+    }
+
+    cout << "Richardson coeffs" << endl;
+    for (int ii=0; ii<coeff_extrap.size() ; ii++ ){
+        cout << coeff_extrap.at(ii) << endl;
+    }
+
+
+    vector<double> input_c;
+    vector<double> input_rho;
+    vector<double> input_mesh;
+    vector<unsigned> input_interf_idcs;
+    vector<double> out_wnum;
+    vector<double> wnum_extrapR;
+    double zc = 0;
+    double zp = 0;
+    double dz = 0;
+    unsigned m_wnum = 1000;
+
+    unsigned n_layers = depths.size();
+    unsigned n_points_total = 0;
+    unsigned n_points_layer = 0;
+// outer loop for Richardson coefficient rr
+    for (unsigned rr = 1; rr <= ordRich; rr++){
+
+        input_c.clear();
+        input_rho.clear();
+        input_interf_idcs.clear();
+        input_mesh.clear();
+        out_wnum.clear();
+
+        input_c.push_back(0);
+        input_rho.push_back(0);
+        n_points_total = 1;
+        zp = 0;
+
+        for (unsigned ll = 0; ll<n_layers; ll++){
+            zc = depths.at(ll);
+            n_points_layer = Ns_points.at(ll)*rr;
+            dz = (zc - zp)/(  n_points_layer  );
+            input_mesh.push_back(  dz  );
+            input_c.at(n_points_total-1) = c1s.at(ll) ;
+            input_rho.at(n_points_total-1) = rhos.at(ll) ;
+
+            n_points_total = n_points_total + n_points_layer;
+
+            for (unsigned kk=1; kk<= n_points_layer; kk++) {
+                input_rho.push_back(rhos.at(ll));
+                input_c.push_back( c1s.at(ll) + (c2s.at(ll) - c1s.at(ll))*kk/n_points_layer );
+            }
+
+            if (ll < n_layers - 1) {
+                input_interf_idcs.push_back(n_points_total-1);
+            }
+            zp = zc;
+        }
+
+        cout << "rr=" << rr << endl;
+
+        out_wnum = compute_wnumbers(omeg, input_c, input_rho,input_interf_idcs, input_mesh,flOnlyTrapped);
+        m_wnum = min(m_wnum, out_wnum.size() );
+
+        if (rr == 1) { wnum_extrapR.assign(m_wnum,0);}
+
+
+        for (unsigned mm=0; mm<m_wnum; mm++ ) {
+            wnum_extrapR.at(mm) = wnum_extrapR.at(mm) + out_wnum.at(mm)*coeff_extrap.at(rr-1);
+        }
+
+
+
+            for (unsigned ii=0; ii<out_wnum.size();  ii++) {
+
+                cout << ii << "->" << out_wnum.at(ii) << endl;
+            }
+
+    }
+
+
+return wnum_extrapR;
+}
+
+
+
+vector<double> compute_wnumbers(           double &omeg, // sound frequency
 											  vector<double> &c,
 											  vector<double> &rho,
-											  vector<int> &interface_idcs,
-											  vector<double> &meshsizes)
+											  vector<unsigned> &interface_idcs,
+											  vector<double> &meshsizes,
+											  unsigned flOnlyTrapped                 // set flOnlyTrapped = 0 to compute all propagating modes, i.e. such that k^2>=0
+											  )
 {
 	// prepare the three diagonals of the matrix to be passed to the EIG function
     // for the c = c_j, j=0... N_points
@@ -101,7 +261,7 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
 	vector<double> ld;
 
     int N_points = c.size();
-    int layer_number = 0;
+    unsigned layer_number = 0;
     double dz = meshsizes.at(layer_number);
     double dz_next = dz;
     double q = 0;
@@ -128,6 +288,9 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
 
     kappamax = omeg/cmin;
     kappamin = omeg/cmax;
+
+    if (flOnlyTrapped == 0 )
+        kappamin = 0;
 
 
     for(int ii=0; ii < N_points-2; ii++ ){
@@ -159,7 +322,7 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
             md.at(ii) = -2*q*( dz_next*dp + dz*dm )/(dz*dz_next) + omeg*omeg*q*( dz*dp*cp*cp + dz_next*dm*cm*cm )/( cp*cp*cm*cm ) ;
             ud.at(ii) = 2*q*dm/dz_next;
 
-            if ( interface_idcs.size() > (unsigned)layer_number )
+            if ( interface_idcs.size() > layer_number )
             {
                 next_interface_idx = interface_idcs.at(layer_number) - 1;
             }
@@ -170,7 +333,6 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
 
             dz = dz_next;
         }
-
     }
 
     // HERE WE CALL THE EIG ROUTINE!!!
@@ -196,19 +358,32 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
 	// fill tridiagonal matrix
 	// make Dirichlet case matrix
 	// На главной диагонали стоят числа -2/(h^2), на под- и над- диагоналях стоят числа 1/(h^2).
-	for ( int ii=0; ii < N_points-2; ii++ ) {
+
+	// matrix is symmetrized: a_{j,j} = m_j; a_{j,j+1} = a_{j+1,j} = \sqrt(u_j*l_{j+1});
+
+
+	/*for ( int ii=0; ii < N_points-2; ii++ ) {
 		A[ii][ii] = md.at(ii);
 
 
 		if ( ii >0 ) {
             A[ii][ii-1] = ld.at(ii);
+            //A[ii-1][ii] = ld.at(ii);
 		}
 
 		if ( ii < N_points-3 ) {
             A[ii][ii+1] = ud.at(ii);
+            //A[ii+1][ii] = ud.at(ii);
 		}
 
+	}*/
+
+    for ( int ii=0; ii < N_points-3; ii++ ) {
+		A[ii][ii] = md.at(ii);
+        A[ii][ii+1] = sqrt(ud.at(ii)*ld.at(ii+1));
+        A[ii+1][ii] = A[ii][ii+1];
 	}
+    A[N_points-3][N_points-3] = md.at(N_points-3);
 
 
     ofstream myFile("thematrixdiags.txt");
@@ -216,6 +391,13 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
         myFile << ld.at(ii) << "  " << md.at(ii) << "  " << ud.at(ii) << endl;
     }
 
+/*    ofstream myFile1("thematrixdiags_A.txt");
+    myFile1 << "W" << "  " << A[1][1] << "  " << A[1][2] << endl;
+    for (int ii=1; ii<N_points-3; ii++){
+        myFile1 << A[ii][ii-1] << "  " << A[ii][ii] << "  " << A[ii][ii+1] << endl;
+    }
+    myFile1 << A[N_points-3][N_points-4] << "  " << A[N_points-3][N_points-3] << "  " << "W" << endl;
+*/
 
 	vector<double> wnumbers;
 
@@ -223,6 +405,8 @@ vector<double> compute_trapped_modes(           double &omeg, // sound frequency
 
 
     //alglib::smatrixevdr( A, N_points-2, 0, 0, kappamin, kappamax, eigen_count, eigenvalues, eigenvectors); // CHECK CALL ARGUMENTS!
+
+
     alglib::smatrixevdr( A, N_points-2, 0, 0, kappamin*kappamin, kappamax*kappamax, eigen_count, eigenvalues, eigenvectors); // CHECK CALL ARGUMENTS!
 
     for (int ii=0; ii<eigen_count; ii++) {
