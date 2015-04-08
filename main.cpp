d0bbd85559f2a87a7edf55209e19d4ae7905ca2a
Oleg Zaikin <zaikin.icc@gmail.com>
2015-04-09 01:53:28 +0800

    VS 2013, compatibility with x64
    VS 2012 -> VS 2013
    changes for compatibility with x64 OS

--- main.cpp
+++ main.cpp
@@ -6,13 +6,17 @@
 // | Authors: Pavel Petrov, Oleg Zaikin                                                |
 // +-----------------------------------------------------------------------------------+
 
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <vector>
 #include <sstream>
 #include <fstream>
 #include <chrono>
 #include <iomanip>
+#include <algorithm>
 #include "linalg.h"
+#include <cmath>
 
 using namespace std;
 
@@ -47,7 +51,7 @@ int main( int argc, char **argv )
 
     cout.precision(15);
 
-    double omeg = 2*3.141592653589793*freq;
+    double omeg = 2*M_PI*freq;
 
     vector<double> input_c;
     vector<double> input_rho;
@@ -102,8 +106,6 @@ int main( int argc, char **argv )
 
     }
 
-
-
     compute_modal_grop_velocities( freqs, deltaf, depths,c1s, c2s, rhos, Ns_points, 1, rord, modal_group_velocities, mode_numbers );
 
     ofstream myFile("mgv.txt");
@@ -122,16 +124,11 @@ int main( int argc, char **argv )
 
             }
             myFile << endl;
-
-
-
-
     }
 
     myFile.close();
 
 */
-	
 	vector<double> depths{90,600};
     vector<double> c1s{1500,2000};
     vector<double> c2s{1500,2000};
@@ -152,7 +149,6 @@ int main( int argc, char **argv )
     double R = 3500;
 
     //SEARCH SPACE
-
     double cb1 = 1600;
     double cb2 = 2600;
     double cbmin, cb_cur;
@@ -170,15 +166,13 @@ int main( int argc, char **argv )
 
     double resmin = 10000;
 
-    ifstream myFileSynth("dtimes_synth_1.txt"); // delays for R = 3500, cw = 1500, cb = 2000, rhow = 1, rhob = 2;
-
-    //stringstream myLineStream;
-
+	// fix start time 
+	chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	
+	ifstream myFileSynth("dtimes_synth_1.txt"); // delays for R = 3500, cw = 1500, cb = 2000, rhow = 1, rhob = 2;
 
     while ( getline(myFileSynth,myLine) ){
-
         stringstream myLineStream(myLine);
-
         myLineStream >> buff;
         freqs.push_back(buff);
         buffvect.clear();
@@ -186,10 +180,8 @@ int main( int argc, char **argv )
         while (!myLineStream.eof()) {
             myLineStream >> buff;
             buffvect.push_back(buff);
-            mode_numbers.push_back( buffvect.size() );
+			mode_numbers.push_back((unsigned)buffvect.size());
         }
-
-
 
         modal_delays.push_back(buffvect);
     }
@@ -200,7 +192,6 @@ int main( int argc, char **argv )
     cout << "Residual is: " << residual << endl;
 
     // brute force minimum search
-
     cout << "BRUTE FORCE MINIMUM SEARCH" << endl;
     cout << "Search space:" << endl;
     cout << cb1 << "< c_b <" << cb2 << ", step" << (cb2 - cb1)/ncb << endl;
@@ -208,24 +199,20 @@ int main( int argc, char **argv )
     cout << R1 << "< Range <" << R2 << ", step" << (R2 - R1)/nR << endl;
 
     for (unsigned ii=0; ii<=ncb; ii++) {
-
         cb_cur = cb1 + ii*(cb2 - cb1)/ncb;
         c1s.at(1) = cb_cur;
         c2s.at(1) = cb_cur;
 
         for (unsigned jj=0; jj<=nrhob; jj++) {
-
             rhob_cur = rhob1 + jj*(rhob2 - rhob1)/nrhob;
             rhos.at(1) = rhob_cur;
 
             for (unsigned kk=0; kk<=nR; kk++) {
-
                 R_cur = R1 + kk*(R2 - R1)/nR;
 
                 residual = compute_modal_delays_residual_uniform( freqs, depths,c1s, c2s, rhos, Ns_points, R_cur, modal_delays, mode_numbers);
 
                 if (residual < resmin) {
-
                     resmin = residual;
                     cbmin = cb_cur;
                     rhobmin = rhob_cur;
@@ -235,29 +222,28 @@ int main( int argc, char **argv )
                     cout << "c_b=" << cbmin << ", rho_b=" << rhobmin << ", R=" << Rmin <<  endl;
 
                 }
-
-
             }
         }
     }
 
+	// fix final time
+	chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+	
     cout << "SEARCH ENDED!" << endl;
     cout << "RESULTING VALUE:" << endl;
     cout << "err=" << resmin << ", parameters:" << endl;
     cout << "c_b=" << cbmin << ", rho_b=" << rhobmin << ", R=" << Rmin <<  endl;
+	cout << "time " << time_span.count() << endl;
 
-
+	ofstream out("out_time");
+	out << time_span.count() << " s";
+	out.close();
+	
 	return 0;
-
-
-
-
-
-
 }
 
 /*
-
 A routine for computing delay residual.
 Arguments:
 1) Environment: five arrays of the same length: depth, c1s, c2s, rhos, Ns_points;
@@ -273,10 +259,7 @@ The routine computes the (uniform) residual (misfit) of experimental data and th
 
 It should be used as follows: for a set of environment models the residual should be computed. The minimal value of the residual indicates
 the most "adequate" model.
-
-
 */
-
 double compute_modal_delays_residual_uniform( vector<double> &freqs,
                                      vector<double> &depths,
                                      vector<double> &c1s,
@@ -301,23 +284,18 @@ double compute_modal_delays_residual_uniform( vector<double> &freqs,
     compute_modal_grop_velocities( freqs, deltaf, depths,c1s, c2s, rhos, Ns_points, flTrappedOnly, rord, modal_group_velocities, mode_numbers );
 
     for (unsigned ii=0; ii<freqs.size();  ii++) {
-
-
-            mnumb = min(mode_numbers.at(ii), experimental_mode_numbers.at(ii) );
-
-            for (unsigned jj=0; jj<mnumb;  jj++) {
-
-                    if (experimental_delays[ii][jj]>0) {
-                            mdelay =  R/modal_group_velocities[ii][jj];
-                            residual = residual + pow(experimental_delays[ii][jj]-mdelay,2);
-                    }
-
+		mnumb = min(mode_numbers.at(ii), experimental_mode_numbers.at(ii) );
+        for (unsigned jj=0; jj<mnumb;  jj++) {
+			if (experimental_delays[ii][jj]>0) {
+				mdelay =  R/modal_group_velocities[ii][jj];
+                residual = residual + pow(experimental_delays[ii][jj]-mdelay,2);
             }
+        }
     }
 
-residual = sqrt(residual);
+	residual = sqrt(residual);
 
-return residual;
+	return residual;
 }
 
 
@@ -333,9 +311,7 @@ int compute_modal_grop_velocities(      vector<double> &freqs,
                                         vector<vector<double>> &modal_group_velocities,
                                         vector<unsigned> &mode_numbers
                                         )
-
 {
-
     mode_numbers.clear();
     modal_group_velocities.clear();
 
@@ -343,18 +319,16 @@ int compute_modal_grop_velocities(      vector<double> &freqs,
     vector<double> out_wnum2;
     vector<double> mgv_ii;
     unsigned nwnum;
-    unsigned nfr = freqs.size();
+	unsigned nfr = (unsigned)freqs.size();
     double omeg1, omeg2;
 
     for (unsigned ii=0; ii<nfr; ii++) {
-
         out_wnum1.clear();
         out_wnum2.clear();
         mgv_ii.clear();
-
-        omeg1 = 2*3.141592653589793*(freqs.at(ii) + deltaf/2);
+        omeg1 = 2*M_PI*(freqs.at(ii) + deltaf/2);
         out_wnum1 = compute_wnumbers_extrap_lin_dz(omeg1,depths,c1s,c2s,rhos,Ns_points,1,ordRich);
-        nwnum = out_wnum1.size();
+		nwnum = (unsigned)out_wnum1.size();
 
         /*
         cout << "f=" << freqs.at(ii) << "Hz" << endl;
@@ -365,9 +339,9 @@ int compute_modal_grop_velocities(      vector<double> &freqs,
         }
         */
 
-        omeg2 = 2*3.141592653589793*(freqs.at(ii) - deltaf/2);
+		omeg2 = 2 *M_PI*(freqs.at(ii) - deltaf / 2);
         out_wnum2 = compute_wnumbers_extrap_lin_dz(omeg2,depths,c1s,c2s,rhos,Ns_points,1,ordRich);
-        nwnum = min(nwnum, out_wnum2.size());
+        nwnum = min(nwnum, (unsigned)out_wnum2.size());
 
         for (unsigned jj=0; jj < nwnum; jj++ )
         {
@@ -378,13 +352,11 @@ int compute_modal_grop_velocities(      vector<double> &freqs,
         mode_numbers.push_back( nwnum );
     }
 
-return 0;
-
+	return 0;
 }
 
 
 /*
-
 General considerations:
 1) It is better to choose mesh in such a way that mesh size is the same for all z. This gives better accuracy!
 (the reason: if the diagonal elements in the upper and lower diagonals vary with row index j, then accuracy is crippled, the
@@ -392,9 +364,7 @@ General considerations:
 2) compute_wnumbers_extrap_lin_dz() makes the number of points within each layer a multiple of 12;
 it is better to set all the numbers to multiple of 12 in advance
 3) Richardson extrapolation of the order 3 gives reasonable accuracy
-
 */
-
 vector<double> compute_wnumbers_extrap(       double &omeg, // sound frequency
 											  vector<double> &depths,
 											  vector<double> &c1s,
@@ -406,7 +376,6 @@ vector<double> compute_wnumbers_extrap(       double &omeg, // sound frequency
 /*  subroutine for computing wavenumbers for a given waveguide structure
     the computation is performed by the FD method for certain meshsize,
     Richardson extrapolation is used to improve the
-
 
 Layer structure:
 
@@ -424,37 +393,29 @@ flOnlyTrapped -- flag to determine the mode subset: set to 0 to compute all prop
 ordRich -- order of the Richardson extrapolation;
 
 the top of the first layer is z=0
-	*/
-
-
+*/
 {
     vector<double> coeff_extrap;
-    if (ordRich == 1) {
-		int tmp_arr[] = {1};
-        coeff_extrap.assign(tmp_arr, tmp_arr + 1);
-    }
-    else if (ordRich == 2){
-		int tmp_arr[] = {-1, 2};
-        coeff_extrap.assign(tmp_arr, tmp_arr + 2);
-    }
-    else if (ordRich == 3){
-		int tmp_arr[] = {0.5, -4, 4.5};
-        coeff_extrap.assign(tmp_arr, tmp_arr + 3);
-        //coeff_extrap.assign({0.1, -0.6, 1.5});
-    }
-    else if (ordRich == 4){
-        coeff_extrap.assign({-1/double(6),4,-13.5,32/double(3)});
-    }
-    else {
-        ordRich = 3;
-        coeff_extrap.assign({0.5, -4, 4.5});
-    }
+	switch (ordRich) {
+	case 1 : 
+		coeff_extrap.assign({ 1 });
+		break;
+	case 2 :
+		coeff_extrap.assign({ -1, 2 });
+		break;
+	case 4 :
+		coeff_extrap.assign({ -1 / double(6), 4, -13.5, 32 / double(3) });
+	default :
+		ordRich = 3;
+		coeff_extrap.assign({ 0.5, -4, 4.5 });
+		//coeff_extrap.assign({0.1, -0.6, 1.5});
+		break;
+	}
 //
 //    cout << "Richardson coeffs" << endl;
 //    for (unsigned ii=0; ii<coeff_extrap.size() ; ii++ ){
 //        cout << coeff_extrap.at(ii) << endl;
 //    }
-
 
     vector<double> input_c;
     vector<double> input_rho;
@@ -467,12 +428,12 @@ the top of the first layer is z=0
     double dz = 0;
     unsigned m_wnum = 1000;
 
-    unsigned n_layers = depths.size();
+	unsigned n_layers = (unsigned)depths.size();
     unsigned n_points_total = 0;
     unsigned n_points_layer = 0;
+
 // outer loop for Richardson coefficient rr
     for (unsigned rr = 1; rr <= ordRich; rr++){
-
         input_c.clear();
         input_rho.clear();
         input_interf_idcs.clear();
@@ -508,45 +469,41 @@ the top of the first layer is z=0
         //cout << "rr=" << rr << endl;
 
         out_wnum2 = compute_wnumbers(omeg, input_c, input_rho,input_interf_idcs, input_mesh,flOnlyTrapped);
-        m_wnum = min(m_wnum, out_wnum2.size() );
+		m_wnum = min(m_wnum, (unsigned)out_wnum2.size());
 
         if (rr == 1) { wnum_extrapR.assign(m_wnum,0);}
-
 
         for (unsigned mm=0; mm<m_wnum; mm++ ) {
             wnum_extrapR.at(mm) = wnum_extrapR.at(mm) + out_wnum2.at(mm)*coeff_extrap.at(rr-1);
         }
 
-
         /*
             for (unsigned ii=0; ii<out_wnum2.size();  ii++) {
-
                 cout << ii << "->" << sqrt(out_wnum2.at(ii)) << endl;
             }
         */
     }
 
-for (unsigned mm=0; mm<m_wnum; mm++ ) {
-    wnum_extrapR.at(mm) = sqrt(wnum_extrapR.at(mm));
+	for (unsigned mm=0; mm<m_wnum; mm++ ) {
+		wnum_extrapR.at(mm) = sqrt(wnum_extrapR.at(mm));
+	}
+
+	return wnum_extrapR;
 }
 
-return wnum_extrapR;
-}
 
 
-
-vector<double> compute_wnumbers_extrap_lin_dz(       double &omeg, // sound frequency
-											  vector<double> &depths,
-											  vector<double> &c1s,
-											  vector<double> &c2s,
-											  vector<double> &rhos,
-											  vector<unsigned> &Ns_points,
-											  unsigned flOnlyTrapped,
-											  unsigned &ordRich)
+vector<double> compute_wnumbers_extrap_lin_dz( double &omeg, // sound frequency
+											   vector<double> &depths,
+											   vector<double> &c1s,
+											   vector<double> &c2s,
+											   vector<double> &rhos,
+											   vector<unsigned> &Ns_points,
+											   unsigned flOnlyTrapped,
+											   unsigned &ordRich )
 /*  subroutine for computing wavenumbers for a given waveguide structure
     the computation is performed by the FD method for certain meshsize,
     Richardson extrapolation is used to improve the
-
 
 Layer structure:
 
@@ -565,12 +522,7 @@ ordRich -- order of the Richardson extrapolation;
 
 the top of the first layer is z=0
 	*/
-
-
 {
-
-
-
     vector<double> input_c;
     vector<double> input_rho;
     vector<double> input_mesh;
@@ -582,38 +534,34 @@ the top of the first layer is z=0
     double dz = 0;
     unsigned m_wnum = 1000;
 
-    unsigned n_layers = depths.size();
+	unsigned n_layers = (unsigned)depths.size();
     unsigned n_points_total = 0;
     unsigned n_points_layer = 0;
 
     vector<double> coeff_extrap;
-    if (ordRich == 1) {
-        coeff_extrap.assign({1});
-    }
-    else if (ordRich == 2){
-        coeff_extrap.assign({ 1.333333333333333, -0.333333333333333});
-    }
-    else if (ordRich == 3){
-        coeff_extrap.assign({1.5, -0.6, 0.1});
-        //coeff_extrap.assign({0.1, -0.6, 1.5});
-    }
-    else if (ordRich == 4){
-       // coeff_extrap.assign({1.595325630252102, -0.788449059052564, 0.216346153846154, -0.023222725045691});
-        coeff_extrap.assign({1.6,  -0.8,   0.228571428571429,  -0.028571428571429});
-    }
-    else {
-        ordRich = 3;
-        coeff_extrap.assign({1.5, -0.6, 0.1});
-    }
-
-
+	switch (ordRich) {
+	case 1 :
+		coeff_extrap.assign({1});
+		break;
+	case 2 :
+		coeff_extrap.assign({1.333333333333333, -0.333333333333333});
+		break;
+	case 4 :
+		// coeff_extrap.assign({1.595325630252102, -0.788449059052564, 0.216346153846154, -0.023222725045691});
+		coeff_extrap.assign({1.6, -0.8, 0.228571428571429, -0.028571428571429});
+		break;
+	default:
+		ordRich = 3;
+		coeff_extrap.assign({1.5, -0.6, 0.1});
+		//coeff_extrap.assign({0.1, -0.6, 1.5});
+		break;
+	}
+	
     // number of points in each layer is multiple of 12
     // this allows us to use nz_ii = nz/ii, ii = 1,2,3,4
     for (unsigned ii=0; ii < n_layers; ii++ ){
         Ns_points.at(ii) = 12*(Ns_points.at(ii)/12);
-
     }
-
 
 //    cout << "Richardson coeffs" << endl;
 //    for (int ii=0; ii<coeff_extrap.size() ; ii++ ){
@@ -622,7 +570,6 @@ the top of the first layer is z=0
 
 // outer loop for Richardson coefficient rr
     for (unsigned rr = 1; rr <= ordRich; rr++){
-
         input_c.clear();
         input_rho.clear();
         input_interf_idcs.clear();
@@ -661,16 +608,13 @@ the top of the first layer is z=0
 //        cout << "rr=" << rr << endl;
 
         out_wnum2 = compute_wnumbers(omeg, input_c, input_rho,input_interf_idcs, input_mesh,flOnlyTrapped);
-        m_wnum = min(m_wnum, out_wnum2.size() );
+		m_wnum = min(m_wnum, (unsigned)out_wnum2.size());
 
         if (rr == 1) { wnum_extrapR.assign(m_wnum,0);}
-
 
         for (unsigned mm=0; mm<m_wnum; mm++ ) {
             wnum_extrapR.at(mm) = wnum_extrapR.at(mm) + out_wnum2.at(mm)*coeff_extrap.at(rr-1);
         }
-
-
 
 //            for (unsigned ii=0; ii<out_wnum2.size();  ii++) {
 //
@@ -679,19 +623,19 @@ the top of the first layer is z=0
 //            cout << endl;
     }
 
-for (unsigned mm=0; mm<m_wnum; mm++ ) {
-    wnum_extrapR.at(mm) = sqrt(wnum_extrapR.at(mm));
-}
-return wnum_extrapR;
+	for (unsigned mm=0; mm<m_wnum; mm++ ) {
+		wnum_extrapR.at(mm) = sqrt(wnum_extrapR.at(mm));
+	}
+	return wnum_extrapR;
 }
 
 
-vector<double> compute_wnumbers(           double &omeg, // sound frequency
-											  vector<double> &c,
-											  vector<double> &rho,
-											  vector<unsigned> &interface_idcs,
-											  vector<double> &meshsizes,
-											  unsigned flOnlyTrapped                 // set flOnlyTrapped = 0 to compute all propagating modes, i.e. such that k^2>=0
+vector<double> compute_wnumbers( double &omeg, // sound frequency
+								 vector<double> &c,
+								 vector<double> &rho,
+								 vector<unsigned> &interface_idcs,
+								 vector<double> &meshsizes,
+								 unsigned flOnlyTrapped                 // set flOnlyTrapped = 0 to compute all propagating modes, i.e. such that k^2>=0
 											  )
 {
 	// prepare the three diagonals of the matrix to be passed to the EIG function
@@ -702,13 +646,12 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
     // for c(interface_idcs.at(k-1)) the value of c is the one BELOW the k-th interface
     //(i.e. for the water-bottom interface at the boundary we take the value from the bottom)
 
-
 	vector<double> md;
 	vector<double> ud;
 	vector<double> ld;
     vector<double> wnumbers2;
 
-    int N_points = c.size();
+	int N_points = (unsigned)c.size();
     unsigned layer_number = 0;
     double dz = meshsizes.at(layer_number);
     double dz_next = dz;//    ofstream myFile("thematrixdiags.txt");
@@ -735,7 +678,6 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
     for( int ii=0; ii < N_points; ii++ ){
         if (c.at(ii) < cmin) { cmin = c.at(ii); }
         if (c.at(ii) > cmax) { cmax = c.at(ii); }
-
     }
 
     kappamax = omeg/cmin;
@@ -744,23 +686,17 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
     if (flOnlyTrapped == 0 )
         kappamin = 0;
 
-
     for(int ii=0; ii < N_points-2; ii++ ){
-
         // ordinary point
-
-
         ud.push_back( 1/(dz*dz) );
         ld.push_back( 1/(dz*dz) );
         md.push_back(-2/(dz*dz)  + omeg*omeg/(c.at(ii+1)*c.at(ii+1)) );
-
 
         // special case of the point at the interface
 
         if (ii == next_interface_idx) {         //ii -- z(ii+1), z(0) = 0
             layer_number = layer_number + 1;    // вообще ii=89 -- вода, в ii=90 -дно,
                                                 // здесь ii = 89 -- интерфейс, уже дно
-
             cp = c.at(ii+1);
             dp = rho.at(ii+1);
             cm = c.at(ii);
@@ -768,7 +704,6 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
             q = 1/( dz_next*dm + dz*dp );
 
             dz_next = meshsizes.at(layer_number);
-
 
             ld.at(ii) = 2*q*dp/dz;
             md.at(ii) = -2*q*( dz_next*dp + dz*dm )/(dz*dz_next) + omeg*omeg*q*( dz*dp*cp*cp + dz_next*dm*cm*cm )/( cp*cp*cm*cm ) ;
@@ -800,16 +735,11 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
     main_diag.setlength(N_points-2);
     second_diag.setlength(N_points-3);
 
-
-
-
-
     for ( int ii=0; ii < N_points-3; ii++ ) {
         second_diag[ii] = sqrt(ud.at(ii)*ld.at(ii+1));
         main_diag[ii] = md.at(ii);
 	}
     main_diag[N_points-3] = md.at(N_points-3);
-
 
 //    ofstream myFile("thematrixdiags.txt");
 //    for (int ii=0; ii<N_points-2; ii++){
@@ -823,17 +753,11 @@ vector<double> compute_wnumbers(           double &omeg, // sound frequency
 //    }
 //    myFile1.close();
 
-
     alglib::smatrixtdevdr(main_diag,  second_diag,  N_points-2,  0, kappamin*kappamin,  kappamax*kappamax, eigen_count,eigenvectors);
 
     for (int ii=0; ii<eigen_count; ii++) {
         wnumbers2.push_back( main_diag[eigen_count-ii-1] );
     }
-
-
-
-
-
 
     return wnumbers2;
 }
