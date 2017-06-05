d1782644730ddc6076c19eca76f3b4f2d88c5616
Oleg Zaikin <zaikin.icc@gmail.com>
2017-06-05 23:32:51 +0800

    - std::

--- main_test.cpp
+++ main_test.cpp
@@ -17,9 +17,11 @@
 #include <algorithm>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {
-	std::string scenarioFileName = "test_scenario.txt";
+	string scenarioFileName = "test_scenario.txt";
 
     //объявить экземпляр
 	sspemdd_sequential sspemdd;
@@ -29,47 +31,57 @@ int main()
     //создание массивов c1s, c2s,...
     //    sspemdd.init();
 
-	std::vector<double> depths, c1s, c2s, rhos, freqs, zr, Rr;
-	std::vector<unsigned> Ns_points;
+	vector<double> depths, c1s, c2s, rhos, freqs, zr, Rr;
+	vector<unsigned> Ns_points;
         double d0, fc, zc, rc;
 
 
     unsigned ppm = 2;
 
-    //std::string ProfileFName = "ssp6m.txt";
+    //string ProfileFName = "ssp6m.txt";
     //sspemdd.load_profile_deep_water(ProfileFName,ppm,depths,c1s,c2s,rhos, Ns_points);
 
     //
-    std::string ProfileFName = "test_pek.txt";
+    string ProfileFName = "test_pek.txt";
 
     sspemdd.load_layers_data(ProfileFName,depths,c1s,c2s,rhos, Ns_points);
 
-    std::cout << " MEDIA PARAMS " << std::endl;
+    cout << " MEDIA PARAMS " << endl;
     for (unsigned jj=0; jj<depths.size(); jj++){
         if (jj==0) { d0 = 0; } else { d0 = depths.at(jj-1); }
-        std::cout << d0 << "-" << depths.at(jj) << "\t" << c1s.at(jj) << "\t" << c2s.at(jj) << "\t" << rhos.at(jj) << "\t" << Ns_points.at(jj) << std::endl;
+        cout << d0 << "-" << depths.at(jj) << "\t" << c1s.at(jj) << "\t" << c2s.at(jj) << "\t" << rhos.at(jj) << "\t" << Ns_points.at(jj) << endl;
     }
-    std::cout << std::endl;
-    std::cout << " ---- " << std::endl;
+    cout << endl;
+    cout << " ---- " << endl;
 
     freqs.clear();
 
-    std::ifstream Myfile;
+    ifstream Myfile;
     Myfile.open("freqs.txt");
+
+	if (!Myfile.is_open()) {
+		cerr << "freqs.txt wasn't opened" << endl;
+		return 1;
+	}
 
     while (!(Myfile.eof()))
 	{
 		Myfile >> fc;
         freqs.push_back(fc);
-		//std::cout << cc << "--" << dc << std::endl;
+		//cout << cc << "--" << dc << endl;
 	}
 	Myfile.close();
 
 
-	std::cout << " FREQUENCIES " << std::endl;
-    std::cout << freqs.at(0) << "--" << freqs.back() << std::endl;
+	cout << " FREQUENCIES " << endl;
+    cout << freqs.at(0) << "--" << freqs.back() << endl;
 
     Myfile.open("srcrec.txt");
+
+	if (!Myfile.is_open()) {
+		cerr << "srcrec.txt wasn't opened" << endl;
+		return 1;
+	}
 
     while (!(Myfile.eof()))
 	{
@@ -77,29 +89,29 @@ int main()
 		Myfile >> zc;
         zr.push_back(zc);
         Rr.push_back(rc);
-		//std::cout << cc << "--" << dc << std::endl;
+		//cout << cc << "--" << dc << endl;
 	}
 	Myfile.close();
 
-	std::cout << " SOURCE " << std::endl;
-    std::cout << "R=" << Rr.at(0) << "; zs=" << zr.at(0) << std::endl;
-    std::cout << " RECEIVERS " << std::endl;
+	cout << " SOURCE " << endl;
+    cout << "R=" << Rr.at(0) << "; zs=" << zr.at(0) << endl;
+    cout << " RECEIVERS " << endl;
     for (unsigned jj=1; jj<zr.size(); jj++){
-        std::cout << "Rr" << jj << "=" << Rr.at(jj) << "\t"  << "zr" << jj << "=" << zr.at(jj) << std::endl;
+        cout << "Rr" << jj << "=" << Rr.at(jj) << "\t"  << "zr" << jj << "=" << zr.at(jj) << endl;
     }
-    std::cout << " ---- " << std::endl;
+    cout << " ---- " << endl;
 
     // ### INITIALIZATION OF VARIABLES
 
     unsigned int ordRich = 3;
 	double omeg1;
-	std::cout.precision(12);
+	cout.precision(12);
 
 	// ### WAVENUMBERS BLOCK ###
 
 
-	std::vector<double> result_vec;
-	std::ofstream ofile("output.txt");
+	vector<double> result_vec;
+	ofstream ofile("output.txt");
 	ofile.precision(12);
 
 	for (unsigned i = 0; i < freqs.size(); i++) {
@@ -107,17 +119,17 @@ int main()
 		result_vec = sspemdd.compute_wnumbers_extrap2(omeg1, depths, c1s, c2s, rhos, Ns_points, 0.6, ordRich);
 		for (unsigned j = 0; j < result_vec.size(); j++)
 			ofile << result_vec.at(j) << "\t";
-		ofile << std::endl;
+		ofile << endl;
 
 
-		std::cout << " f= " << omeg1/(2 * LOCAL_M_PI) << std::endl;
-        std::cout << " ---- " << std::endl;
-		std::cout << " k_j " << std::endl;
+		cout << " f= " << omeg1/(2 * LOCAL_M_PI) << endl;
+        cout << " ---- " << endl;
+		cout << " k_j " << endl;
         for (unsigned jj=0; jj<result_vec.size(); jj++){
-            std::cout << result_vec.at(jj) << "\t";
+            cout << result_vec.at(jj) << "\t";
         }
-        std::cout << std::endl;
-        std::cout << " ---- " << std::endl;
+        cout << endl;
+        cout << " ---- " << endl;
 
 	}
 	ofile.close();
@@ -130,8 +142,8 @@ int main()
 
     // ### GROUP VELOCITIES BLOCK ###
 
-    std::vector<std::vector<double>> mgvs;
-    std::vector<unsigned> mnumbs;
+    vector<vector<double>> mgvs;
+    vector<unsigned> mnumbs;
     sspemdd.compute_modal_grop_velocities2(freqs, 0.005, depths, c1s, c2s, rhos, Ns_points,-1.0,ordRich, mgvs,mnumbs);
 
     ofile.open("mgvs2.txt");
@@ -141,17 +153,17 @@ int main()
 
 		for (unsigned j = 0; j < mnumbs.at(i); j++)
 			ofile << mgvs.at(i).at(j) << "\t";
-		ofile << std::endl;
+		ofile << endl;
 
 
-		std::cout << " f= " << freqs.at(i) << std::endl;
-        std::cout << " ---- " << std::endl;
-		std::cout << " vg_j " << std::endl;
+		cout << " f= " << freqs.at(i) << endl;
+        cout << " ---- " << endl;
+		cout << " vg_j " << endl;
         for (unsigned jj=0; jj<mnumbs.at(i); jj++){
-            std::cout << mgvs.at(i).at(jj) << "\t";
+            cout << mgvs.at(i).at(jj) << "\t";
         }
-        std::cout << std::endl;
-        std::cout << " ---- " << std::endl;
+        cout << endl;
+        cout << " ---- " << endl;
 
 	}
 	ofile.close();
@@ -165,17 +177,17 @@ int main()
 
 		for (unsigned j = 0; j < mnumbs.at(i); j++)
 			ofile << mgvs.at(i).at(j) << "\t";
-		ofile << std::endl;
+		ofile << endl;
 
 
-		std::cout << " f= " << freqs.at(i) << std::endl;
-        std::cout << " ---- " << std::endl;
-		std::cout << " vg_j " << std::endl;
+		cout << " f= " << freqs.at(i) << endl;
+        cout << " ---- " << endl;
+		cout << " vg_j " << endl;
         for (unsigned jj=0; jj<mnumbs.at(i); jj++){
-            std::cout << mgvs.at(i).at(jj) << "\t";
+            cout << mgvs.at(i).at(jj) << "\t";
         }
-        std::cout << std::endl;
-        std::cout << " ---- " << std::endl;
+        cout << endl;
+        cout << " ---- " << endl;
 
 	}
 	ofile.close();
@@ -187,10 +199,10 @@ int main()
 
     double freq_threshold = 0.5;
 
-    std::ofstream Pfile("PHelm.txt");
-    std::vector<std::complex<double>> PHelm;
+    ofstream Pfile("PHelm.txt");
+    vector<complex<double>> PHelm;
     for (unsigned ff=0; ff<freqs.size(); ff++ ){
-        std::cout << "f=" << freqs.at(ff) << ": " << std::endl;
+        cout << "f=" << freqs.at(ff) << ": " << endl;
 
         if (freqs.at(ff)>freq_threshold) {
             PHelm = sspemdd.compute_cpl_pressure(freqs.at(ff),depths,c1s,c2s,rhos,Ns_points,Rr,zr,1/sqrt(2),ordRich);
@@ -202,13 +214,13 @@ int main()
                 Pfile << "\t" << PHelm.at(jj).real() << "  " << PHelm.at(jj).imag();
 
             }
-            Pfile << std::endl;
+            Pfile << endl;
         } else {
 
             for (unsigned jj=0; jj<Rr.size(); jj++ ){
                 Pfile << "\t" << 0 << "  " << 0;
             }
-            Pfile << std::endl;
+            Pfile << endl;
         }
 
     }
@@ -218,12 +230,12 @@ int main()
 
 
 
-	std::cout << "test passed" << std::endl;
+	cout << "test passed" << endl;
 
-    std::complex<double> U(0.0,LOCAL_M_PI/4);
-    std::cout << sqrt(2)*exp(U) << std::endl;
-    std::cout << sqrt(2)*exp(U) + Iu << std::endl;
-    U = (std::complex<double>(4.5,2.7));
+    complex<double> U(0.0,LOCAL_M_PI/4);
+    cout << sqrt(2)*exp(U) << endl;
+    cout << sqrt(2)*exp(U) + Iu << endl;
+    U = (complex<double>(4.5,2.7));
 
 
 	return 0;
