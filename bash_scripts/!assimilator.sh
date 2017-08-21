1cb4efcdf33ecd3f2b4250f8e76911792088b4bd
Oleg Zaikin <zaikin.icc@gmail.com>
2017-08-22 02:06:11 +0800

    + bash scripts

---
+++ bash_scripts/!assimilator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "cat and sort all strings from all files"
+counter=0
+for file in `find ./ -name "*-wu*"`
+do
+    sed -i -e '$a\' $file
+done
+echo "residual cb rhob R tau cws_array" > ./!results
+find ./ -name "*-wu*" | xargs cat | sort > ./!results
---
+++ bash_scripts/!boinc_copy_results.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+for f in ./39* 
+do 
+    mv $f ../results/39/
+done
+for f in ./310* 
+do 
+    mv $f ../results/310/ 
+done
+for f in ./43* 
+do 
+    mv $f ../results/43/ 
+done
+for f in ./44* 
+do 
+    mv $f ../results/44/ 
+done
+for f in ./503_* 
+do 
+    mv $f ../results/503/ 
+done
