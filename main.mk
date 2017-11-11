965f5f484b1b466943b39041ca92c1a65330e2db
V.G. Bulavintsev <golem.md@gmail.com>
2017-11-11 14:33:47 +0800

    * Dirs cleanup

--- main.mk
+++ main.mk
@@ -3,6 +3,6 @@ CXXFLAGS := -g -O3 -Wall -pipe -std=c++11
 BUILD_DIR := build
 TARGET_DIR := out
 #INCDIRS = ext/easyloggingpp
-INCDIRS = ext/easyloggingpp ext/bbox/COMPI/ ext/bbox/snowgoose/  ext/easyloggingpp/  src/tests_ahw/  ext/bbox/LOCSEARCH/ ext/bbox/BBSEARCH/
+INCDIRS = ext/easyloggingpp ext/bbox/COMPI/ ext/bbox/snowgoose/  ext/easyloggingpp/  ext/bbox/LOCSEARCH/ ext/bbox/BBSEARCH/
 
 SUBMAKEFILES := ext/easyloggingpp/easylogging++.mk src/libcambala/libcambala.mk src/cambala/cambala.mk #src/tests/rescalc.mk
