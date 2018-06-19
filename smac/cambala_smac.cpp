3f1c9a9683af1159d06bdab204920bd3c5be831c
Oleg Zaikin <zaikin.icc@gmail.com>
2018-06-20 01:15:46 +1000

    don't deal with depths parameters

--- smac/cambala_smac.cpp
+++ smac/cambala_smac.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
 			sstream << str_val;
 			sstream >> point.cws[cw_index];
 		}
-		else if ((word.size() >= 3) && (word[0] == '-') && (word[1] == 'd')) {
+		/*else if ((word.size() >= 3) && (word[0] == '-') && (word[1] == 'd')) {
 			word = word.substr(2, word.size() - 2);
 			unsigned d_index;
 			istringstream(word) >> d_index;
@@ -53,17 +53,17 @@ int main(int argc, char **argv)
 			//sstream << str_val.substr(1, str_val.size() - 2);
 			sstream << str_val;
 			sstream >> point.depths[d_index];
-		}
+		}*/
 	}
 
 	cout << "cws :\n";
 	for (unsigned i = 0; i < point.cws.size(); i++)
 		cout << point.cws[i] << " ";
 	cout << endl;
-	cout << "depths :\n";
+	/*cout << "depths :\n";
 	for (unsigned i = 0; i < point.depths.size(); i++)
 		cout << point.depths[i] << " ";
-	cout << endl;
+	cout << endl;*/
 	
 	CAMBALA_sequential CAMBALA_seq;
 	CAMBALA_seq.verbosity = 0;
@@ -73,24 +73,25 @@ int main(int argc, char **argv)
 	point.R    = CAMBALA_seq.R1;
 	point.rhob = CAMBALA_seq.rhob1;
 	point.tau  = CAMBALA_seq.tau1;
-	vector<vector<double>> depths_vec = CAMBALA_seq.createDepthsArray();
-	cout << "depths_vec size " << depths_vec.size() << endl;
+	point.depths = CAMBALA_seq.d1_arr;
+	//vector<vector<double>> depths_vec = CAMBALA_seq.createDepthsArray();
+	//cout << "depths_vec size " << depths_vec.size() << endl;
 
 	point.depths.push_back(CAMBALA_seq.h);
 	point.depths.push_back(CAMBALA_seq.H);
-	cout << "full depths :\n";
+	cout << "point depths :\n";
 	for (unsigned i = 0; i < point.depths.size(); i++)
 		cout << point.depths[i] << " ";
 	cout << endl;
 	
 	double val = 0;
-	vector<vector<double>>::iterator it = find(depths_vec.begin(), depths_vec.end(), point.depths);
+	/*vector<vector<double>>::iterator it = find(depths_vec.begin(), depths_vec.end(), point.depths);
 	if (it == depths_vec.end())
 		val = 1e100;
-	else {
-		CAMBALA_seq.init(point.depths);
-		val = CAMBALA_seq.fillDataComputeResidual(point);
-	}
+	else {*/
+	CAMBALA_seq.init(point.depths);
+	val = CAMBALA_seq.fillDataComputeResidual(point);
+	//}
 	cout << "Result for SMAC: SUCCESS, 0, 0, " << val << ", 0";
 	
 	return 0;
