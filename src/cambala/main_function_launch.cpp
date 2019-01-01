c486662b5f5b2102d37ef236aa07ffb300b64613
Oleg Zaikin <zaikin.icc@gmail.com>
2019-01-01 12:28:17 +0300

    compilation fix

--- src/cambala/main_function_launch.cpp
+++ src/cambala/main_function_launch.cpp
@@ -26,12 +26,12 @@ int main(int argc, char **argv)
 	string scenarioFileName;
 	search_space_point point;
 	// input data
-	point.cb = 2050;
-	point.R = 4925;
-	point.rhob = 1;
-	point.tau = 0;
-	point.cws = { 1538.5, 1538.5, 1538.5, 1538.5, 1536.5, 1534.5 };
-	point.depths = { 20, 40, 60, 80, 98, 108, 500 };
+	point.cb = 1730;
+	point.R = 6080;
+	point.rhob = 2.4;
+	point.tau = 0.063;
+	point.cws = { 1452, 1450, 1448 };
+	point.depths = { 5, 20, 37, 500 };
 
 	if (argc == 1) {
         cerr << "Usage: program scenario_file \n";
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
 	CAMBALA_seq.directPointCalc( point );
 	direct_point_calc_time = cpu_time() - direct_point_calc_time;
 	cout << "direct_point_calc_time " << direct_point_calc_time << endl;
-	CAMBALA_seq.reportFinalResult();
+	CAMBALA_seq.reportCurrentResult(true);
 
 	cout << "test done" << endl;
 	cout << "final time " << cpu_time() - start_time << endl;
